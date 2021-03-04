464a5f362798b28a98b084b07068518227ef8453
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2021-03-04 14:35:56 +0100

    update

--- run_batch.sh
+++ run_batch.sh
@@ -1,13 +1,14 @@
 #!bin/bash
 
 # Job submission for DESY HTCondor - copy to project directory and adapt to your needs
-JOBNAME="EVGEN"
-DSIDS="<enter DSIDs here>" #Range of DSIDs in job option directory
+JOBNAME="EVGEN_bsm4tops"
+DSIDS="150000 150017" #Range of DSIDs in job option directory
 EVENTS=10000               #Events per job
 NJOBS=5                    #Number of jobs per DSID
 RUNTIME="03:00:00"         #Run time per job HH:MM:SS
 MEMORY=2000                #Memory per job in MB
 
+cd batch_submission
 COMMAND="python SubmitMC/python/submit.py --jobName ${JOBNAME} --engine HTCONDOR --eventsPerJob ${EVENTS} --nJobs ${NJOBS} -R ${DSIDS} --noBuildJob --accountinggroup af-atlas --evgen_runtime ${RUNTIME} --evgen_memory ${MEMORY}"
 echo $COMMAND
 $COMMAND
