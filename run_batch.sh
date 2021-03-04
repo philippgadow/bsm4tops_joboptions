7e14773c087fb047534872ffef3a338646c00c59
Paul Philipp Gadow <paul.philipp.gadow@cern.ch>
2021-03-04 16:05:02 +0100

    fix htcondor submission with models dir

--- run_batch.sh
+++ run_batch.sh
@@ -7,8 +7,9 @@ EVENTS=10000               #Events per job
 NJOBS=5                    #Number of jobs per DSID
 RUNTIME="03:00:00"         #Run time per job HH:MM:SS
 MEMORY=2000                #Memory per job in MB
+MODELSDIR=$PWD/models
 
 cd batch_submission
-COMMAND="python SubmitMC/python/submit.py --jobName ${JOBNAME} --engine HTCONDOR --eventsPerJob ${EVENTS} --nJobs ${NJOBS} -R ${DSIDS} --noBuildJob --accountinggroup af-atlas --evgen_runtime ${RUNTIME} --evgen_memory ${MEMORY}"
+COMMAND="python SubmitMC/python/submit.py --jobName ${JOBNAME} --engine HTCONDOR --eventsPerJob ${EVENTS} --nJobs ${NJOBS} -R ${DSIDS} --noBuildJob --modelsDir ${MODELSDIR} --accountinggroup af-atlas --evgen_runtime ${RUNTIME} --evgen_memory ${MEMORY}"
 echo $COMMAND
 $COMMAND
